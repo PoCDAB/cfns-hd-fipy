e18aeaa58478e95aa4136c51f561d4a23fac3d4d
FrankMonteny8 <frankmonteny02@gmail.com>
2021-11-15 15:22:17 +0100

    removed unused libraries to speed up uploading the files. They will be added back later when the code is done. added the testfile and necessary library. added smal improvements and fixes some small bugs.

--- Wifi.py
+++ Wifi.py
@@ -1,8 +1,11 @@
-from network import WLAN
-import uping
+from network import WLAN # type: ignore the line
+import uping # type: ignore the line
 import socket
 import json
-import machine
+import machine # type: ignore the line
+
+class NotAbleToConnectError(Exception):
+    pass
 
 """
     Class to utilize WiFi to connect with the network on board of the ship
@@ -15,6 +18,8 @@ class WiFi:
         self.static_address = ('ASSIGN_STATIC_IP', 'SUBENTMASK', 'DEFAULT_GATEWAY', 'DNS')
         # example self.static_address = ('192.168.178.81', '255.255.255.0', '192.168.1.10', '8.8.8.8')
         self.client = socket.socket()
+        self.target_host = "driel.rh.nl"
+        self.target_port = 65532
 
     """
         This function tries to connect to a network within 10 seconds. If it succeeds it will return True otherwise it will return False.
@@ -38,28 +43,26 @@ class WiFi:
         return False
     
     """
-        This function checks if the server with url driel.rh.nl is reachable or not.
+        This function checks if the server with can reach the host at a port
     """ 
-    def has_reach(self):
+    def has_reach(self, host, port, quiet=True):
         try:
-            response = uping.ping(socket.getaddrinfo("driel.rh.nl", 65532)[0][4][0])
-            return True if response[0] == 4 and response[1] > 1 else False
+            response = uping.ping(host=socket.getaddrinfo(host, port)[0][4][0], quiet=quiet)
+            return True if response[0] >= 1 and response[1] >= 1 else False
         except OSError:
-            print("Not able to reach")
+            print("Not able to reach {}".format(host))
             return False
-        return False
-    
-    """
-        This function sends the confirmation to driel.rh.nl using Wifi. 
-    """
 
     def init_socket(self):
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect(self):
-        self.client.connect(socket.getaddrinfo("driel.rh.nl", 65532)[0][4])
+    def connect(self, host, port):
+        try:
+            self.client.connect(socket.getaddrinfo(host, port)[0][4])
+        except OSError:
+            raise(NotAbleToConnectError)
     
-    def disconect(self, max_msg_length):
+    def disconnect(self, max_msg_length):
         close_message = json.dumps({"DISCONNECT": True}) # This is the message used to close the connection
         length_close_message = pad_msg_length(max_msg_length, len(close_message))
 
@@ -67,6 +70,12 @@ class WiFi:
         self.client.send(length_close_message)
         self.client.send(close_message.encode())
 
+        # Close the socket
+        self.client.close()
+
+    """
+        This function sends the confirmation to driel.rh.nl using Wifi. 
+    """
     def send(self, confirmation, max_msg_length):
         # Prepare the messages
         confirmation = json.dumps(confirmation)
@@ -79,14 +88,13 @@ class WiFi:
         # Receive the confirmation from the server
         reply_length = self.client.recv(max_msg_length).decode()
         
-        if len(confirmation_length) == 0:
+        if len(reply_length) == 0:
             return False
 
         reply = self.client.recv(int(reply_length)).decode()
         reply = json.loads(reply)
 
         return reply
-
 
 """
     pad the var msg_length to the padding size. 
@@ -96,3 +104,4 @@ def pad_msg_length(padding_size, msg_length):
     msg_length = str(msg_length).encode()
     msg_length += b' ' * (padding_size - len(msg_length))
     return msg_length
+
